fdf45b8372218fb32748abfcd968f9b7c4435544
nguyenminhtu <tuunguyen2795@gmail.com>
2020-02-05 10:23:15 +0700

    Update config 2 3

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const component = render(<App />);
-  expect(component).not.toMatchSnapshot();
+  expect(component).toMatchSnapshot();
 });
